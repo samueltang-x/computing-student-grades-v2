2d8f276298b431e0bf2a4435c6fadaeb43ff81e2
exantng <xiangyuan.tang@ericsson.com>
2018-10-22 23:43:51 +0800

    add the generic Hanlde class to refer multiple classes

--- computing-student-grades-v2/student_info.h
+++ computing-student-grades-v2/student_info.h
@@ -69,4 +69,49 @@ private:
 	Core* cp;
 };
 
+
+template <class T> class Handle {
+public:
+	Handle() : p(0) {}    // default constructor
+	Handle(T* t) : p(t) {}
+	~Handle() { delete p; }    // desctructor
+
+	// copy constructor
+	Handle(const Handle& s) : p(0) { if (s.p) p = s.p->clone(); }
+
+	// asignment operator
+	Handle& operator=(const Handle&);
+
+	operator bool() const { return p; }
+	T& operator*() const;
+	T* operator->() const;
+
+private:
+	T* p;
+};
+
+template<class T>
+Handle<T>& Handle<T>::operator=(const Handle& rhs) {
+	if (&rhs != this) {
+		delept p;
+		p = rhs.p ? rhs.p->clone() : 0;
+	}
+
+	return *this;
+}
+
+template<class T>
+T& Handle<T>::operator*() const {
+	if (p)
+		return *p;
+	throw std::runtime_error("unbound Handle");
+}
+
+template<class T>
+T* Handle<T>::operator->() const {
+	if (p)
+		return p;
+	throw std::runtime_error("unbound Handle");
+}
+
 #endif
